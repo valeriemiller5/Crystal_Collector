14196172ee894b9913d609529ecaf43fbdb866f7
vflores <valerie.flores64@gmail.com>
2018-07-14 17:29:15 -0700

    finished reset function in jQuery, changed button name in html, and updated margins in css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,79 @@
 $(document).ready(function() {
 
-var goalNumber = Math.floor(Math.random() * 120) + 19;
+var goalNumber = Math.floor(Math.random() * 102 + 19);
 $("#randomNumber").html(goalNumber);
+console.log(goalNumber)
 
-var redCrystal = "";
-var blueCrystal = "";
-var greenCrystal = "";
-var yellowCrystal = "";
+var redCrystal = Math.floor(Math.random() * 12 + 1);
+console.log(redCrystal)
+var blueCrystal = Math.floor(Math.random() * 12 + 1);
+console.log(blueCrystal)
+var greenCrystal = Math.floor(Math.random() * 12 + 1);
+console.log(greenCrystal)
+var yellowCrystal = Math.floor(Math.random() * 12 + 1);
+console.log(yellowCrystal)
 
 var calcNumber = 0;
+var youWin = 0;
+var youLose = 0;
 
-$("#redClick").click(function() {
-    redCrystal = Math.floor(Math.random() * 12) + 1;
+function newRound() {
+  if (calcNumber == goalNumber){
+    youWin++;
+    $("#wins").append(youWin);
+    alert("You Win!");
+    resetGame();
+  }
+  else if (calcNumber > goalNumber){
+    youLose++;
+    $("#losses").append(youLose);
+    alert("Game Over");
+    resetGame();
+  }
+};
+
+function resetGame() {
+  goalNumber = Math.floor(Math.random() * 102 + 19);
+  $("#randomNumber").text(goalNumber);
+  redCrystal = Math.floor(Math.random() * 12 + 1);
+  blueCrystal = Math.floor(Math.random() * 12 + 1);
+  greenCrystal = Math.floor(Math.random() * 12 + 1);
+  yellowCrystal = Math.floor(Math.random() * 12 + 1);
+  calcNumber = 0;
+  $("#guessCalc").html(calcNumber);
+};
+
+$("#redClick").click(function() { 
     calcNumber = calcNumber + redCrystal;
-    $("#guessCalc").html(calcNumber); 
-        if (calcNumber == goalNumber){
-          wins();
-        }
-        else if (calcNumber > goalNumber){
-          losses();
-        }
+    console.log(calcNumber)
+    $("#guessCalc").html(calcNumber);
+    newRound(); 
   });
 $("#blueClick").click(function() {
-    blueCrystal = Math.floor(Math.random() * 12) + 1;
     calcNumber = calcNumber + blueCrystal;
-    $("#guessCalc").html(calcNumber); 
-        if (calcNumber == goalNumber){
-          wins();
-        }
-        else if (calcNumber > goalNumber){
-          losses();
-        }
+    console.log(calcNumber)
+    $("#guessCalc").html(calcNumber);
+    newRound(); 
   }); 
 $("#greenClick").click(function() {
-    greenCrystal = Math.floor(Math.random() * 12) + 1;
     calcNumber = calcNumber + greenCrystal;
-    $("#guessCalc").html(calcNumber); 
-        if (calcNumber == goalNumber){
-          wins();
-        }
-        else if (calcNumber > goalNumber){
-          losses();
-        }
+    console.log(calcNumber)
+    $("#guessCalc").html(calcNumber);
+    newRound(); 
   });
 $("#yellowClick").click(function() {
-    yellowCrystal = Math.floor(Math.random() * 12) + 1;
     calcNumber = calcNumber + yellowCrystal;
-    $("#guessCalc").html(calcNumber); 
-        if (calcNumber == goalNumber){
-          wins();
-        }
-        else if (calcNumber > goalNumber){
-          losses();
-        }
+    console.log(calcNumber)
+    $("#guessCalc").html(calcNumber);
+    newRound(); 
   });
+
+$("#startGame").click(function() {
+  resetGame();
+  youWin = 0;
+  $("#wins").html(youWin);
+  youLose = 0;
+  $("#losses").html(youLose);
+})
 
 });
